dbcf1bee58240b3f6f86111ef06ff0349cc606a6
Psycho Mantys <psycho.mantys@gmail.com>
2020-11-13 16:59:06 -0300

    Change RUNTIME_USER to root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,8 @@
 #	done
 #}
 
+RUNTIME_USER=root
+
 parse_env(){
 	if [ -r "$1" ] ; then
 		while IFS="=" read key value  ; do
@@ -25,45 +27,45 @@ bootstrap_conf(){
 	# Fixing permissions
 	find /app -not -user discourse -exec chown discourse:discourse {} \+
 
-	gosu discourse bundle config build.nokogiri --use-system-libraries
-	gosu discourse bundle config set deployment 'true'
-	gosu discourse bundle config set without 'test:development'
+	gosu "${RUNTIME_USER}" bundle config build.nokogiri --use-system-libraries
+	gosu "${RUNTIME_USER}" bundle config set deployment 'true'
+	gosu "${RUNTIME_USER}" bundle config set without 'test:development'
 
-	gosu discourse bundle config set DISCOURSE_DB_HOST "${POSTGRES_HOST}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_DB_HOST "${POSTGRES_HOST}"
 	export DISCOURSE_DB_HOST="${POSTGRES_HOST}"
-	gosu discourse bundle config set DISCOURSE_DB_PORT "${POSTGRES_PORT}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_DB_PORT "${POSTGRES_PORT}"
 	export DISCOURSE_DB_PORT="${POSTGRES_PORT}"
-	gosu discourse bundle config set DISCOURSE_DB_NAME "${POSTGRES_DB_NAME}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_DB_NAME "${POSTGRES_DB_NAME}"
 	export DISCOURSE_DB_NAME="${POSTGRES_DB_NAME}"
-	gosu discourse bundle config set DISCOURSE_DB_USERNAME "${POSTGRES_USER}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_DB_USERNAME "${POSTGRES_USER}"
 	export DISCOURSE_DB_USERNAME="${POSTGRES_USER}"
-	gosu discourse bundle config set DISCOURSE_DB_PASSWORD "${POSTGRES_PASSWORD}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_DB_PASSWORD "${POSTGRES_PASSWORD}"
 	export DISCOURSE_DB_PASSWORD="${POSTGRES_PASSWORD}"
 
-	gosu discourse bundle config set DISCOURSE_REDIS_HOST "${REDIS_HOST}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_REDIS_HOST "${REDIS_HOST}"
 	export DISCOURSE_REDIS_HOST="${REDIS_HOST}"
-	gosu discourse bundle config set DISCOURSE_REDIS_PORT "${REDIS_PORT}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_REDIS_PORT "${REDIS_PORT}"
 	export DISCOURSE_REDIS_PORT="${REDIS_PORT}"
 	if [ "${REDIS_PASSWORD}" ] ; then
-		gosu discourse bundle config set DISCOURSE_REDIS_PASSWORD "${REDIS_PASSWORD}"
+		gosu "${RUNTIME_USER}" bundle config set DISCOURSE_REDIS_PASSWORD "${REDIS_PASSWORD}"
 		export DISCOURSE_REDIS_PASSWORD="${REDIS_PASSWORD}"
 	fi
 	if [ "${REDIS_DB}" ] ; then
-		gosu discourse bundle config set DISCOURSE_REDIS_DB "${REDIS_DB}"
+		gosu "${RUNTIME_USER}" bundle config set DISCOURSE_REDIS_DB "${REDIS_DB}"
 		export DISCOURSE_REDIS_DB="${REDIS_DB}"
 	fi
 
-	gosu discourse bundle config set DISCOURSE_SMTP_ADDRESS "${SMTP_HOST}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_ADDRESS "${SMTP_HOST}"
 	export DISCOURSE_SMTP_ADDRESS="${SMTP_HOST}"
-	gosu discourse bundle config set DISCOURSE_SMTP_PORT "${SMTP_PORT}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_PORT "${SMTP_PORT}"
 	export DISCOURSE_SMTP_PORT="${SMTP_PORT}"
-	gosu discourse bundle config set DISCOURSE_SMTP_USER_NAME "${SMTP_USER}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_USER_NAME "${SMTP_USER}"
 	export DISCOURSE_SMTP_USER_NAME="${SMTP_USER}"
-	gosu discourse bundle config set DISCOURSE_SMTP_PASSWORD "${SMTP_PASSWORD}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_PASSWORD "${SMTP_PASSWORD}"
 	export DISCOURSE_SMTP_PASSWORD="${SMTP_PASSWORD}"
-	gosu discourse bundle config set DISCOURSE_SMTP_AUTHENTICATION "${SMTP_AUTHENTICATION}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_AUTHENTICATION "${SMTP_AUTHENTICATION}"
 	export DISCOURSE_SMTP_AUTHENTICATION="${SMTP_AUTHENTICATION}"
-	gosu discourse bundle config set DISCOURSE_SMTP_ENABLE_START_TLS "${SMTP_ENABLE_START_TLS}"
+	gosu "${RUNTIME_USER}" bundle config set DISCOURSE_SMTP_ENABLE_START_TLS "${SMTP_ENABLE_START_TLS}"
 	export DISCOURSE_SMTP_ENABLE_START_TLS="${SMTP_ENABLE_START_TLS}"
 }
 
@@ -75,16 +77,16 @@ if [[ "start" == *"$1"* ]]; then
 	/wait-for "${REDIS_HOST}:${REDIS_PORT}" -- echo Redis Server: "${REDIS_HOST}:${REDIS_PORT}" started
 	bootstrap_conf
 
-	#exec gosu discourse bundle exec rake assets:precompile
+	#exec gosu "${RUNTIME_USER}" bundle exec rake assets:precompile
 	if [ "${DISCOURSE_DONT_INIT_DATABASE}" != "true" ] ; then
-		gosu discourse bundle exec rake db:create || echo 'ERROR: bundle exec rake db:create'
+		gosu "${RUNTIME_USER}" bundle exec rake db:create || echo 'ERROR: bundle exec rake db:create'
 	fi
-	gosu discourse bundle exec rake db:migrate || echo 'ERROR: bundle exec rake db:migrate'
+	gosu "${RUNTIME_USER}" bundle exec rake db:migrate || echo 'ERROR: bundle exec rake db:migrate'
 
 	if [ "${DISCOURSE_DONT_INIT_SU}" != "true" -a ! -f /discourse_su_created ] ; then
 		#echo -e "${DISCOURSE_SU_EMAIL}\n${DISCOURSE_SU_PASSWORD}\n${DISCOURSE_SU_PASSWORD}\nY"
 		echo -e "${DISCOURSE_SU_EMAIL}\n${DISCOURSE_SU_PASSWORD}\n${DISCOURSE_SU_PASSWORD}\nY" \
-			| gosu discourse bundle exec rake admin:create
+			| gosu "${RUNTIME_USER}" bundle exec rake admin:create
 		touch /discourse_su_created
 	fi
 
@@ -92,27 +94,27 @@ if [[ "start" == *"$1"* ]]; then
 		echo 'SiteSetting.content_security_policy = false' | /docker-entrypoint.sh bundle exec rails c
 	fi
 	if [ "${DISCOURSE_DONT_PRECOMPILE}" != "true" -a ! -f /discourse_precompiled ] ; then
-		gosu discourse bundle exec rake assets:precompile
+		gosu "${RUNTIME_USER}" bundle exec rake assets:precompile
 		touch /discourse_precompiled
 	fi
-	#gosu discourse mailcatcher --http-ip 0.0.0.0
+	#gosu "${RUNTIME_USER}" mailcatcher --http-ip 0.0.0.0
 
 	tail -f log/* &
 
 	(
 		while true ; do
-			gosu discourse bundle config set RAILS_MAX_THREADS 3
-			RAILS_MAX_THREADS=3 gosu discourse bundle exec sidekiq -c 2 -v -L /dev/stdout -C config/sidekiq.yml -q low,2 -q critical,8 -q default,4 -q ultra_low
+			gosu "${RUNTIME_USER}" bundle config set RAILS_MAX_THREADS 3
+			RAILS_MAX_THREADS=3 gosu "${RUNTIME_USER}" bundle exec sidekiq -c 2 -v -L /dev/stdout -C config/sidekiq.yml -q low,2 -q critical,8 -q default,4 -q ultra_low
 		done
 	) &
 
 	sed -i s'/8, 32/4, 16/' config/puma.rb
 
-	exec gosu discourse bundle exec rails server --binding="0.0.0.0" --port="${DISCOURSE_PORT}"
+	exec gosu "${RUNTIME_USER}" bundle exec rails server --binding="0.0.0.0" --port="${DISCOURSE_PORT}"
 elif [[ "bundle" == "$1" ]]; then
 	bootstrap_conf
 
-	exec gosu discourse $@
+	exec gosu "${RUNTIME_USER}" $@
 elif [[ "healthcheck" == "$1" ]]; then
 	nc -z -w5 127.0.0.1 "${DISCOURSE_PORT}" || exit 1
 	exit 0
